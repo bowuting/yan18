7fc3a187e70f4dcb6c87029f0eb85ba0f50293b0
zizhou_Mac <863508032@qq.com>
2016-03-03 17:57:51 +0800

    穆攀傻逼

---
+++ 03/code_me/36.php
@@ -0,0 +1,15 @@
+<?php
+  //索引数组  关联数组
+
+  $arr = array(0=>'东',1=>'西',2=>'南');
+  //键不必有特殊  叫索引数组
+
+  $stu = array('name'=>'bowuting','age'=>22);
+  //字符串做键，一般能体现该单元的内容，如age->年龄
+
+  $arr = array(0 => a, 1 =>array('stu' =>'li' ,'bag' =>array('yuwen','shuxue') ));
+  //多维数组，数组里面还是数组。
+
+  print_r($arr);
+
+ ?>
---
+++ 03/code_me/37.php
@@ -0,0 +1,10 @@
+<?php
+
+  $arr = array('a','b','c');
+  print_r($arr);
+
+
+  $arr = array('' => , );
+
+
+ ?>
