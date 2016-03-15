45fc757b799dd8db686f941e5a4e93b6b66343ac
bowuting <bowuting@gmail.com>
2016-03-15 22:03:46 +0800

    在html里嵌套php  反过来好像不行
    在html里嵌套php  反过来好像不行

--- 03/code_me/60_du.php
+++ 03/code_me/60_du.php
@@ -17,5 +17,5 @@ $rs = mysql_query($sql,$conn);
 
 $row = mysql_fetch_assoc($rs);
 ?>
-<h1>  <?php echo $row['title']; ?> </h1>
+<h1>  <?php echo $row['title']; ?> </h1>    //注意这里的HTML标签  要放在第一个php标签之外，，，，只能在html里嵌套php  反过来好像不行
 <p>   <?php  echo $row['content']; ?></p>
