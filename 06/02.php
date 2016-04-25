3647fdf618780fcac28a3ba336bf302678e0f83b
bowuting <bowuting@qq.com>
2016-04-26 04:01:47 +0800

    太失败了

---
+++ 06/02.php
@@ -0,0 +1,20 @@
+<?php
+
+class People{
+  public $name = 'nobody';
+  public $height = '30';
+  public function cry(){
+    echo '呱呱坠地';
+  }
+
+}
+
+$a = new People();
+print_r($a);
+
+echo $a->name,'<br />',$a->height,'<br />';
+
+$a->cry();
+
+
+ ?>
