88182076cbd0e156c5cf654e5e230fe3f6754f75
bowuting <bowuting@gmail.com>
2016-05-31 16:22:50 +0800

    递归学习

--- 07/145_2.php
+++ 07/145_2.php
@@ -1,15 +1,17 @@
 <?php
 
-function read($path){
+
+function read($path,$lev=1){
   $dh = opendir($path);
   while (($row = readdir($dh)) !== false) {
     //过滤掉. 和 ..
     if(($row == '.') || ($row == '..')){
       continue;
     }
-    echo $row,'<br />';
+    //echo str_repeat("&nbsp;&nbsp;",$lev*4),$row,'<br />';
+    echo '|——',str_repeat("——",$lev),$row,'<br />';
     if (is_dir($path.'/'.$row)) {
-      read($path.'/'.$row);
+      read($path.'/'.$row,$lev+1);
     }
   }
 
---
+++ 07/145_3.php
@@ -0,0 +1,8 @@
+<?php
+
+echo str_repeat("&nbsp;&nbsp;",4);
+echo "--";
+
+
+
+ ?>
