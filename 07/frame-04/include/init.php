ce5ff4382af7228c99618e455e9ed96adb2de720
bowuting <bowuting@gmail.com>
2016-06-08 01:53:19 +0800

    文件整理

--- 07/frame-04/include/init.php
+++ 07/frame-04/include/init.php
@@ -1,45 +1,53 @@
-
 <?php
+/****
+燕十八 公益PHP讲堂
+
+论  坛: http://www.zixue.it
+微  博: http://weibo.com/Yshiba
+YY频道: 88354001
+****/
+
+
+
 /*
-初始化文件
-作用：框架初始化
+file init.php
+作用:框架初始化
 */
-defined('ADC') || exit('ADC Denied');
-define ('ROOT',str_replace('\\','/',dirname(dirname(__FILE__))) . '/');
-//ROOT文件夹
-//echo ROOT;
-
-// require (ROOT.'include/db.class.php');   //引入数据库类
-// require (ROOT.'include/conf.class.php'); //引入配置文件类
-// require (ROOT.'include/log.class.php');
- require (ROOT.'include/lib_base.php');
-// require (ROOT.'include/mysql.class.php');
-//
-// require (ROOT.'model/Model.class.php');
-// require (ROOT.'model/TestModel.class.php');
 
 
-function __autoload($class){
-  if (strtolower(substr($class,-5)) == 'model') {
-    require (ROOT.'model/' .$class. '.class.php');
-  } else {
-    require (ROOT.'include/'.$class.'.class.php');
-  }
+// 初始化当前的绝对路径
+// 换成正斜线是因为 win/linux都支持正斜线,而linux不支持反斜线
+
+defined('ACC')||exit('ACC Denied');
+
+
+define('ROOT',str_replace('\\','/',dirname(dirname(__FILE__))) . '/');
+define('DEBUG',true);
+
+
+require(ROOT . 'include/lib_base.php');
+
+function __autoload($class) {
+    if(strtolower(substr($class,-5)) == 'model') {
+        require(ROOT . 'Model/' . $class . '.class.php');
+    } else {
+        require(ROOT . 'include/' . $class . '.class.php');
+    }
 }
 
 
-define ('DEBUG',true);                   //定义调试模式
-if (defined('DEBUG')) {
-  error_reporting(E_ALL);
-} else {
-  error_reporting(0);
-}
 
-$_GET  = _addslashes($_GET);
+// 过滤参数,用递归的方式过滤$_GET,$_POST,$_COOKIE,暂时不会
+$_GET = _addslashes($_GET);
 $_POST = _addslashes($_POST);
 $_COOKIE = _addslashes($_COOKIE);
 
-                                          //递归过滤参数  $_GET  $_POST
-                                          //日志记录功能
 
- ?>
+// 设置报错级别
+
+
+if(defined('DEBUG')) {
+    error_reporting(E_ALL);
+} else {
+    error_reporting(0);
+}
