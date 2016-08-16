0c0b715f3517acaf1cbae40f5f6054bd77990014
bowutingMac <bowuting@gmail.com>
2016-08-16 11:35:11 +0800

    0306

--- 08/04_0305/comp/7b7a8b8b2b52c15f421a0b415d8afdefe63605c0.file.05.html.php
+++ 08/04_0305/comp/7b7a8b8b2b52c15f421a0b415d8afdefe63605c0.file.05.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1-DEV, created on 2016-08-15 10:00:58
+<?php /* Smarty version Smarty-3.1-DEV, created on 2016-08-16 11:06:17
          compiled from "./temp/05.html" */ ?>
 <?php /*%%SmartyHeaderCode:35256520957b122293322d7-89422502%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '7b7a8b8b2b52c15f421a0b415d8afdefe63605c0' => 
     array (
       0 => './temp/05.html',
-      1 => 1471226413,
+      1 => 1471316757,
       2 => 'file',
     ),
   ),
@@ -21,6 +21,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   array (
     'name' => 0,
     'age' => 0,
+    'arr' => 0,
+    'stu' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -34,6 +36,14 @@ $_valid = $_smarty_tpl->decodeProperties(array (
       <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
 
       <?php echo $_smarty_tpl->tpl_vars['age']->value;?>
+
+
+      <?php echo $_smarty_tpl->tpl_vars['arr']->value['country']['prov']['city'];?>
+
+
+      <?php echo $_smarty_tpl->tpl_vars['stu']->value[0];?>
+
+      <?php echo $_smarty_tpl->tpl_vars['stu']->value[1];?>
 
 
   </body>
